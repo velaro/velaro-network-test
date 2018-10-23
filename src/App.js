2ac2993ab7375117e40e015157c1792bc3bb1fa6
Ryan Whitmire <ryanw51@gmail.com>
2018-10-23 11:40:05 -0400

    bring back multiple signalr servers

--- src/App.js
+++ src/App.js
@@ -37,25 +37,21 @@ const Production = () => (
     <VisitorApiTest endpoint="https://api-visitor-us-east.velaro.com/v1/test/ok" />
     <CdnTest endpoint="https://eastprodcdn.azureedge.net/bundles/velaro.inline.js" />
     <EngagementHubTest
-      title="Engagement Hub"
-      endpoint="https://signalr-engagement-us-east.velaro.com"
-    />
-    {/* <EngagementHubTest
       title="Engagement Hub 1"
-      endpoint="http://signalr-us-prod.eastus.cloudapp.azure.com"
+      endpoint="https://signalr-east-prod-1.velaro.com"
     />
     <EngagementHubTest
       title="Engagement Hub 2"
-      endpoint="http://signalr-us-prod-2.eastus.cloudapp.azure.com"
+      endpoint="https://signalr-east-prod-2.velaro.com"
     />
     <EngagementHubTest
       title="Engagement Hub 3"
-      endpoint="http://signalrusprd3.eastus.cloudapp.azure.com"
+      endpoint="https://signalr-east-prod-3.velaro.com"
     />
     <EngagementHubTest
       title="Engagement Hub 4"
-      endpoint="http://signalrusprd4.eastus.cloudapp.azure.com"
-    /> */}
+      endpoint="https://signalr-east-prod-4.velaro.com"
+    />
     <BandwidthTest />
   </div>
 );
